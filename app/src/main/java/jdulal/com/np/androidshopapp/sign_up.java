1f130a7760b1e0edf8be18b0ef8006c0f09fbd01
Mukeem <shiekhabdulmukeemWgmail.com>
2020-02-18 16:24:18 +0530

    Initial commit

---
+++ app/src/main/java/jdulal/com/np/androidshopapp/sign_up.java
@@ -0,0 +1,13 @@
+package jdulal.com.np.androidshopapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sign_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
